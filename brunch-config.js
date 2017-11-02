3d2212036c633808351fe7b9dfe5919619da1ec5
Stéphane SIGUR <stephane@sigur.net>
2017-11-02 10:54:33 +0100

    [Skin] Config Brunch / Sass + Normalize

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,5 @@
 // See http://brunch.io for documentation.
 exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+      javascripts: {joinTo: 'app.js'},
+      stylesheets: { joinTo: { 'app.css' : /^app/}}
 };

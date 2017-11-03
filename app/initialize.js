9d5e37c9758936668538adda1449b75fd108ce23
Stéphane SIGUR <stephane@sigur.net>
2017-11-03 13:23:58 +0100

    [Skin] Strate 1 (Carousel) : Slide 1, Mise en place & animation POI

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // do your setup here
-  console.log('Initialized app');
+
+  // POI Animations
+  var el = document.getElementsByClassName('poi');
+  for (var i = 0 ; i < el.length; i++) {
+      el[i].addEventListener('click', function () {
+          if (this.classList.contains('active')) {
+              this.classList.remove('active');
+          } else {
+              this.classList.add('active');
+          }
+      });
+  }
 });
